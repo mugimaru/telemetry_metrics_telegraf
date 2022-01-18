6334b0d8bebebfcb07fd6ae648942424d9e0767f
mugimaru <pochi.73@gmail.com>
2022-01-18 11:07:02 +0400

    mix format

--- lib/telemetry_metrics_telegraf/app_config.ex
+++ lib/telemetry_metrics_telegraf/app_config.ex
@@ -12,18 +12,10 @@ defmodule TelemetryMetricsTelegraf.AppConfig do
 
   ## Configuration options
   * `:period` - aggregation period (default: #{inspect(@default_config[:period])})
-  * `:histogram_reset` - histogram aggregator `reset` option (default: #{
-    inspect(@default_config[:histogram_reset])
-  })
-  * `:histogram_cumulative` - histogram aggregator `cumulative` option (default: #{
-    inspect(@default_config[:histogram_cumulative])
-  })
-  * `:summary_stats` - stats list for summary metric (basicstats aggregator). In context of this option `nil` means "all stats" (default: #{
-    inspect(@default_config[:summary_stats])
-  })
-  * `:log_telegraf_config_on_start` - if the option is set to true `TelemetryMetricsTelegraf` logs telegraf aggregators config on start (default: #{
-    @default_config[:log_telegraf_config_on_start]
-  })
+  * `:histogram_reset` - histogram aggregator `reset` option (default: #{inspect(@default_config[:histogram_reset])})
+  * `:histogram_cumulative` - histogram aggregator `cumulative` option (default: #{inspect(@default_config[:histogram_cumulative])})
+  * `:summary_stats` - stats list for summary metric (basicstats aggregator). In context of this option `nil` means "all stats" (default: #{inspect(@default_config[:summary_stats])})
+  * `:log_telegraf_config_on_start` - if the option is set to true `TelemetryMetricsTelegraf` logs telegraf aggregators config on start (default: #{@default_config[:log_telegraf_config_on_start]})
 
   See [telegraf aggregators repo](https://github.com/influxdata/telegraf/tree/master/plugins/aggregators) for more details.
 
